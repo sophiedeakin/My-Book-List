b27355ffba10cf691ecdb3e64bd715abe8d6a167
Sophie Deakin <deakinsophie@gmail.com>
2024-05-08 11:49:26 +0100

    Modified app.py, added port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect,  url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -52,4 +53,5 @@ def delete(id):
 if __name__ == '__main__':
     with app.app_context():
          db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))     
+    app.run(debug=True, host='0.0.0.0', port=port)
